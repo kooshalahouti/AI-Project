4d81c39bd61a4af3d80100e1b3189d3df4565fe5
koosha lahouti <klahouti81@gmail.com>
2024-01-21 00:30:06 +0330

    second

--- run.py
+++ run.py
@@ -1,46 +1,37 @@
-from template import NaiveBayesClassifier
-import csv
-import re
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
+import csv
+from template import NaiveBayesClassifier
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 def preprocess(tweet_string):
-    # clean the data and tokenize it
-    # You may need to install the 'nltk' library for tokenization
+    tweet_string = tweet_string.lower()
+    tweet_string = re.sub(r'[^\w\s]', '', tweet_string)
+    features = word_tokenize(tweet_string)
     stop_words = set(stopwords.words('english'))
-
-    # Remove special characters, URLs, and split into words
-    tweet_string = re.sub(r'http\S+|www\S+|https\S+', '',
-                          tweet_string, flags=re.MULTILINE)
-    tweet_string = re.sub(r'\@\w+|\#', '', tweet_string)
-    words = word_tokenize(tweet_string)
-
-    # Remove stopwords and non-alphabetic words
-    features = [word.lower() for word in words if word.isalpha()
-                and word.lower() not in stop_words]
+    features = [word for word in features if word not in stop_words]
 
     return features
 
 
 def load_data(data_path):
-    # load the csv file and return the data
     data = []
 
     with open(data_path, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
-        next(reader)  # skip header
+        next(reader)
         for row in reader:
-            # assuming the tweet is in the first column
-            features = preprocess(row[0])
-            label = row[1]  # assuming the label is in the second column
-            data.append((features, label))
+            tweet, label = row[0], row[1]
+            data.append((preprocess(tweet), label))
 
     return data
 
 
-# Set the path to your training data CSV file
-train_data_path = "path/to/your/train_data.csv"
+train_data_path = "train_data.csv"
 classes = ['positive', 'negative', 'neutral']
 nb_classifier = NaiveBayesClassifier(classes)
 nb_classifier.train(load_data(train_data_path))
--- template.py
+++ template.py
@@ -1,66 +1,63 @@
-# Naive Bayes 3-class Classifier
-# Authors: Baktash Ansari - Sina Zamani
-
-# complete each of the class methods
 from collections import defaultdict
 import math
-import csv
 
 
 class NaiveBayesClassifier:
 
     def __init__(self, classes):
-        # initialization:
-        # inputs: classes(list) --> list of label names
-        # class_word_counts --> frequency dictionary for each class
-        # class_counts --> number of instances of each class
-        # vocab --> all unique words
         self.classes = classes
-        self.class_word_counts = {c: defaultdict(int) for c in classes}
-        self.class_counts = {c: 0 for c in classes}
+        self.class_word_counts = {label: {} for label in classes}
+        self.class_counts = {label: 0 for label in classes}
         self.vocab = set()
 
     def train(self, data):
-        # training process:
-        # inputs: data(list) --> each item of list is a tuple
-        # the first index of the tuple is a list of words and the second index is the label(positive, negative, or neutral)
-
         for features, label in data:
+            if label not in self.classes:
+                self.classes.append(label)
+                self.class_word_counts[label] = {}
+                self.class_counts[label] = 0
             self.class_counts[label] += 1
             for word in features:
-                self.class_word_counts[label][word] += 1
+                if word not in self.class_word_counts[label]:
+
+                    self.class_word_counts[label][word] = 1
+                else:
+                    self.class_word_counts[label][word] += 1
                 self.vocab.add(word)
-            # Your Code
 
     def calculate_prior(self):
-        # calculate log prior
-        # you can add some attributes to this method
+        total_instances = sum(self.class_counts.values())
+        log_prior = {}
 
-        # Your Code
-        log_prior = {c: math.log(
-            self.class_counts[c] / sum(self.class_counts.values())) for c in self.classes}
+        for label, count in self.class_counts.items():
+            if count > 0 and total_instances > 0:
+                log_prior[label] = math.log(count / total_instances)
+            else:
+                log_prior[label] = float('-inf')
+
         return log_prior
 
     def calculate_likelihood(self, word, label):
-        # calculate likelihhood: P(word | label)
-        # return the corresponding value
-
         smoothing_factor = 1
-        word_count = self.class_word_counts[label][word]
-        total_words_in_class = sum(self.class_word_counts[label].values())
-        likelihood = math.log(word_count + smoothing_factor) / \
-            (total_words_in_class + smoothing_factor * len(self.vocab))
+        word_count = self.class_word_counts[label].get(
+            word, 0) + smoothing_factor
+        total_words = sum(self.class_word_counts[label].values(
+        )) + smoothing_factor * len(self.vocab)
+        likelihood = math.log(word_count / total_words)
         return likelihood
 
     def classify(self, features):
-        # predict the class
-        # inputs: features(list) --> words of a tweet
         log_prior = self.calculate_prior()
-        log_likelihoods = {c: sum(self.calculate_likelihood(
-            word, c) for word in features) for c in self.classes}
-        scores = {c: log_prior[c] + log_likelihoods[c] for c in self.classes}
-        best_class = max(scores, key=scores.get)
+        best_class = None
+        max_posterior = float('-inf')
+
+        for label in self.classes:
+            posterior = log_prior[label]
+            for word in features:
+                posterior += self.calculate_likelihood(word, label)
+
+            if posterior > max_posterior:
+                max_posterior = posterior
+                best_class = label
+
         return best_class
-
-
-# Good luck :)
