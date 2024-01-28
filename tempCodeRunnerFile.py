4d6b697746abde399ee8dfddff45e6243a81b27b
koosha lahouti <klahouti81@gmail.com>
2024-01-29 02:51:03 +0330

    final

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,18 @@
- else:
-                print(f"Skipping invalid row: {row}")
+curacy(classifier):
+#     eval_data_path = "eval_data.csv"
+#     eval_data = load_data(eval_data_path)
+
+#     cnt = 0
+#     total = len(eval_data)
+
+#     for features, true_label in eval_data:
+#         predicted = classifier.classify(features)
+#         if predicted == true_label:
+#             cnt += 1
+
+#     accuracy = cnt / total
+#     return accuracy
+
+# accuracy_on_eval_data = accuracy(nb_classifier)
+
+# print(f"Accuracy on eval: {accuracy_on_eval_data:.2%}")
--- template.py
+++ template.py
@@ -8,7 +8,7 @@ class NaiveBayesClassifier:
         self.classes = classes
         self.class_word_counts = defaultdict(lambda: defaultdict(int))
         self.class_counts = defaultdict(int)
-        self.vocab = set()        
+        self.vocab = set()
 
     def train(self, data):
         for features, label in data:
@@ -19,7 +19,8 @@ class NaiveBayesClassifier:
 
     def calculate_prior(self):
         total_instances = sum(self.class_counts.values())
-        log_prior = {label: math.log(count / total_instances) for label, count in self.class_counts.items()}
+        log_prior = {label: math.log(count / total_instances)
+                     for label, count in self.class_counts.items()}
         # for label in self.classes:
         #     if label not in log_prior:
         #         log_prior[label] = float('-inf')
@@ -27,17 +28,18 @@ class NaiveBayesClassifier:
 
     def calculate_likelihood(self, word, label):
         alpha = 1.0
-        numerator = self.class_word_counts[label][word] + alpha
-        denominator = sum(self.class_word_counts[label].values()) + (alpha * len(self.vocab))
-        return math.log(numerator / denominator)
+        numerator = self.class_word_counts[label].get(word, 0) + alpha
+        denominator = sum(
+            self.class_word_counts[label].values()) + (alpha * len(self.vocab))
+        return (numerator / denominator)
 
     def classify(self, features):
         log_prior = self.calculate_prior()
-        class_scores = {label: log_prior.get(label, 0.0) for label in self.classes}
-
-        for word in features:
-            for label in self.classes:
-                class_scores[label] += self.calculate_likelihood(word, label)
-
+        class_scores = {label: log_prior.get(
+            label, 0.0) for label in self.classes}
+        for label in self.classes:
+            for word in features:
+                likelihood = self.calculate_likelihood(word, label)
+                class_scores[label] += math.log(likelihood)
         best_class = max(class_scores, key=class_scores.get)
         return best_class
